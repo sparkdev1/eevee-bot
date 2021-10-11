43db64c2c4c91030eae861fa9a094bcdbf380d27
FelipeCiranda <felipe.silveira@cirandacultural.com.br>
2021-10-11 16:24:09 -0300

    Improved Anilist + Jikan API's, return error needs fix

--- mal.mjs
+++ mal.mjs
@@ -1,103 +1,93 @@
+import request from 'request'
 import anilist from 'anilist-node';
 const Anilist = new anilist();
 import fetch from 'node-fetch'
 //Anilist.media.anime(21708).then(data => {
-//     console.log(data);
-// });
-function getRandomCharacter(min, max) {
-    return Math.ceil(Math.random() * (max - min) + min);
-}
-var randomInt = 4;
+    //     console.log(data);
+    // });
+    function getRandomCharacter(min, max) {
+        return Math.ceil(Math.random() * (max - min) + min);
+    }
 
+var dropInfo = []
 
+async function getFROMAPI() {
+    var randomInt = getRandomCharacter(1, 1000)
 
-// let characterID = getRandomCharacter(1,50000);
-// var drop = []
-// Anilist.people.character(73935).then(data => {
-//     drop.push({'name': data.name.english, 'image': data.image.large, 'animeTitle': data.media['0'].title.english});
-
-//     Anilist.people.character(17).then(data2 => {
-//         drop.push({'name': data2.name.english, 'image': data2.image.large, 'animeTitle': data2.media['0'].title.english});
-
-//         Anilist.people.character(126071).then(data3 => {
-//             drop.push({'name': data3.name.english, 'image': data3.image.large, 'animeTitle': data3.media['0'].title.english});
-//             console.log(drop)
-//         }) 
-//     })
-// })
-
-// Here we define our query as a multi-line string
-// Storing it in a separate .graphql/.gql file is also possible
-var randomInt = getRandomCharacter(1, 200)
-
-var query = `
-query ($randomInt: Int){
-    Page (page: $randomInt, perPage: 1) {
-      pageInfo {
-        total
-      }
-      media(type: ANIME, sort: FAVOURITES) {
-        title {
-            romaji
+    var query = `
+    query ($randomInt: Int){
+        Page (page: $randomInt, perPage: 1) {
+        pageInfo {
+            total
         }
-        characters (sort: ID){
-            edges{
-                node {
-                    name {
-                        first
-                        last
-                    }
-                    image {
-                        large
+        media(type: ANIME, sort: FAVOURITES_DESC) {
+            title {
+                romaji
+            }
+            characters (sort: ID){
+                edges{
+                    node {
+                        id
+                        name {
+                            first
+                            last
+                        }
+                        image {
+                            large
+                        }
                     }
                 }
             }
         }
-      }
+        }
     }
-  }
-`;
+    `;
 
-// Define our query variables and values that will be used in the query request
+    // Define our query variables and values that will be used in the query request
 
-var variables = {
-        randomInt: randomInt
+    var variables = {
+            randomInt: randomInt
+        }
+        // Define the config we'll need for our Api request
+    var url = 'https://graphql.anilist.co',
+        options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({
+                query: query,
+                variables: variables
+            })
+        };
+
+
+    await fetch(url, options).then(await handleResponse)
+    .then(await handleData)
+
+    function handleResponse(response) {
+        return response.json().then(function(json) {
+            return response.ok ? json : Promise.reject(json);
+        });
     }
-    // Define the config we'll need for our Api request
-var url = 'https://graphql.anilist.co',
-    options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        },
-        body: JSON.stringify({
-            query: query,
-            variables: variables
+
+    async function handleData(data) {
+
+        var Char = data.data.Page.media[0].characters.edges[getRandomCharacter(0, data.data.Page.media[0].characters.edges.length - 1)];
+        var Title = data.data.Page.media[0].title;
+        request(`https://api.jikan.moe/v3/character/${Char.node.id}/pictures`, async function (error, response, body) {
+            var Pics = JSON.parse(body).pictures[getRandomCharacter(0, JSON.parse(body).pictures.length - 1)];
+            dropInfo.push(Pics)
+            dropInfo.push(Title)
+            dropInfo.push(Char)
+            return dropInfo
         })
-    };
-
-
-// Make the HTTP Api request
-await fetch(url, options).then(handleResponse)
-    .then(handleData)
-    .catch(handleError);
-
-
-function handleResponse(response) {
-    return response.json().then(function(json) {
-        return response.ok ? json : Promise.reject(json);
-    });
+        return dropInfo
+    }
+    return dropInfo
 }
 
-async function handleData(data) {
-    data.data.Page.media[0].characters.edges.forEach(element => {
-        console.log(element)
-    });
-    await console.log(data.data.Page.media[0].title)
-    console.log(data.data.Page.media)
-}
-
-function handleError(error) {
-    console.error(error);
-}
+ console.log(await getFROMAPI())
+ console.log(await getFROMAPI())
+ console.log(await getFROMAPI())
